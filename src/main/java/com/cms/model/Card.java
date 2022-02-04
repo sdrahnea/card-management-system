96000568e29da3d7bc13a65fc710c26b57d77444
Sergiu Drahnea <s.drahnea@gmail.com>
2022-02-04 18:38:55 +0200

    add card event enum

--- src/main/java/com/cms/model/Card.java
+++ src/main/java/com/cms/model/Card.java
@@ -2,9 +2,20 @@ package com.cms.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 @Data
 public class Card {
 
     private CardCategory cardCategory;
+    private CardStatus cardStatus;
+    private BigDecimal amount;
+
+    private Calendar createdDate;
+    private Calendar updatedDate;
+    private Calendar closedDate;
+    private Calendar orderedDate;
+    private Calendar shippedDate;
 
 }
