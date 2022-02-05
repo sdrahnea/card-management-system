d5db36e3218351051dabadf731b5006b7e97ef9a
Sergiu Drahnea <s.drahnea@gmail.com>
2022-02-05 09:30:10 +0200

    add card controller default methods

---
+++ src/main/java/com/cms/controller/CardController.java
@@ -0,0 +1,20 @@
+package com.cms.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardController {
+
+    public void order() {}
+
+    public void ship() {}
+
+    public void block() {}
+
+    public void lock() {}
+
+    public void unlock() {}
+
+    public void confirm() {}
+
+}
